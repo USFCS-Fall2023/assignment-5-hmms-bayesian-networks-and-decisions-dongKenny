5b8937264db928f7ef9134c53ca6435b9a583787
dongKenny <kdong4@dons.usfca.edu>
2023-11-07 03:56:51 -0800

    Queries and KeyPresent

--- carnet.py
+++ carnet.py
@@ -1,71 +1,98 @@
 from pgmpy.models import BayesianNetwork
 from pgmpy.inference import VariableElimination
-
-car_model = BayesianNetwork(
-    [
-        ("Battery", "Radio"),
-        ("Battery", "Ignition"),
-        ("Ignition","Starts"),
-        ("Gas","Starts"),
-        ("Starts","Moves")
-    ]
-)
-
-# Defining the parameters using CPT
 from pgmpy.factors.discrete import TabularCPD
 
-cpd_battery = TabularCPD(
-    variable="Battery", variable_card=2, values=[[0.70], [0.30]],
-    state_names={"Battery":['Works',"Doesn't work"]},
-)
 
-cpd_gas = TabularCPD(
-    variable="Gas", variable_card=2, values=[[0.40], [0.60]],
-    state_names={"Gas":['Full',"Empty"]},
-)
+def car_queries():
+    car_model = BayesianNetwork(
+        [
+            ("Battery", "Radio"),
+            ("Battery", "Ignition"),
+            ("Ignition", "Starts"),
+            ("Gas", "Starts"),
+            ("Starts", "Moves"),
+            ("KeyPresent", "Starts")
+        ]
+    )
 
-cpd_radio = TabularCPD(
-    variable=  "Radio", variable_card=2,
-    values=[[0.75, 0.01],[0.25, 0.99]],
-    evidence=["Battery"],
-    evidence_card=[2],
-    state_names={"Radio": ["turns on", "Doesn't turn on"],
-                 "Battery": ['Works',"Doesn't work"]}
-)
+    # Defining the parameters using CPT
+    cpd_battery = TabularCPD(
+        variable="Battery", variable_card=2, values=[[0.70], [0.30]],
+        state_names={"Battery": ['Works', "Doesn't work"]},
+    )
 
-cpd_ignition = TabularCPD(
-    variable=  "Ignition", variable_card=2,
-    values=[[0.75, 0.01],[0.25, 0.99]],
-    evidence=["Battery"],
-    evidence_card=[2],
-    state_names={"Ignition": ["Works", "Doesn't work"],
-                 "Battery": ['Works',"Doesn't work"]}
-)
+    cpd_gas = TabularCPD(
+        variable="Gas", variable_card=2, values=[[0.40], [0.60]],
+        state_names={"Gas": ['Full', "Empty"]},
+    )
 
-cpd_starts = TabularCPD(
-    variable="Starts",
-    variable_card=2,
-    values=[[0.95, 0.05, 0.05, 0.001], [0.05, 0.95, 0.95, 0.9999]],
-    evidence=["Ignition", "Gas"],
-    evidence_card=[2, 2],
-    state_names={"Starts":['yes','no'], "Ignition":["Works", "Doesn't work"], "Gas":['Full',"Empty"]},
-)
+    cpd_radio = TabularCPD(
+        variable="Radio", variable_card=2,
+        values=[[0.75, 0.01], [0.25, 0.99]],
+        evidence=["Battery"],
+        evidence_card=[2],
+        state_names={"Radio": ["turns on", "Doesn't turn on"],
+                     "Battery": ['Works', "Doesn't work"]}
+    )
 
-cpd_moves = TabularCPD(
-    variable="Moves", variable_card=2,
-    values=[[0.8, 0.01],[0.2, 0.99]],
-    evidence=["Starts"],
-    evidence_card=[2],
-    state_names={"Moves": ["yes", "no"],
-                 "Starts": ['yes', 'no'] }
-)
+    cpd_ignition = TabularCPD(
+        variable="Ignition", variable_card=2,
+        values=[[0.75, 0.01], [0.25, 0.99]],
+        evidence=["Battery"],
+        evidence_card=[2],
+        state_names={"Ignition": ["Works", "Doesn't work"],
+                     "Battery": ['Works', "Doesn't work"]}
+    )
 
+    cpd_starts = TabularCPD(
+        variable="Starts",
+        variable_card=2,
+        values=[[0.99, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01], [0.01, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99]],
+        evidence=["Ignition", "Gas", "KeyPresent"],
+        evidence_card=[2, 2, 2],
+        state_names={"Starts": ['yes', 'no'], "Ignition": ["Works", "Doesn't work"], "Gas": ['Full', "Empty"], "KeyPresent": ["yes", "no"]},
+    )
 
-# Associating the parameters with the model structure
-car_model.add_cpds( cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, cpd_moves)
+    cpd_moves = TabularCPD(
+        variable="Moves", variable_card=2,
+        values=[[0.8, 0.01], [0.2, 0.99]],
+        evidence=["Starts"],
+        evidence_card=[2],
+        state_names={"Moves": ["yes", "no"],
+                     "Starts": ['yes', 'no']}
+    )
 
-car_infer = VariableElimination(car_model)
+    cpd_key = TabularCPD(
+        variable="KeyPresent", variable_card=2, values=[[0.70], [0.30]],
+        state_names={"KeyPresent": ['yes', "no"]},
+    )
 
-print(car_infer.query(variables=["Moves"],evidence={"Radio":"turns on", "Starts":"yes"}))
+    # Associating the parameters with the model structure
+    car_model.add_cpds(cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, cpd_moves, cpd_key)
 
+    car_infer = VariableElimination(car_model)
 
+    q_1 = car_infer.query(variables=["Battery"], evidence={"Moves": "yes"})
+    q_2 = car_infer.query(variables=["Starts"], evidence={"Radio": "Doesn't turn on"})
+
+    q_3 = car_infer.query(variables=["Radio"], evidence={"Battery": "Works"})
+    q_3_2 = car_infer.query(variables=["Radio"], evidence={"Battery": "Works", 'Gas': 'Full'})
+
+    q_4 = car_infer.query(variables=["Ignition"], evidence={"Moves": "no"})
+    q_4_2 = car_infer.query(variables=["Ignition"], evidence={"Moves": "no", 'Gas': 'Empty'})
+
+    q_5 = car_infer.query(variables=["Starts"], evidence={"Radio": "turns on", 'Gas': 'Full'})
+
+    q_6 = car_infer.query(variables=["Starts"], evidence={"Ignition": "Works", 'Gas': 'Full', "KeyPresent": "yes"})
+
+    print(q_1)
+    print(q_2)
+
+    print(q_3)
+    print(q_3_2)
+
+    print(q_4)
+    print(q_4_2)
+
+    print(q_5)
+    print(q_6)
--- submission.py
+++ submission.py
@@ -1,10 +1,17 @@
 from HMM import HMM
+from alarm import alarm_queries
+from carnet import car_queries
 
 
 def main():
-    hmm = HMM()
-    hmm.load('partofspeech.browntags.trained')
-    print(hmm.generate(12))
+    # hmm = HMM()
+    # hmm.load('partofspeech.browntags.trained')
+    # print(hmm.generate(12))
+    #
+    # alarm_queries()
+    car_queries()
+    print('Probability unchanged for the radio working given the battery is working if the we discover the car has gas')
+    print('Probability affected for the ignition failing given the car does not move and we observe there is no gas')
 
 
 if __name__ == '__main__':
